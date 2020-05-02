83ac3f253c9f48c6b658894c85f1a3d6d8016df0
irving jacome <irving2291@gmail.com>
2020-05-02 02:04:46 -0500

    models y controllers

---
+++ app/Src/CatalogList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Src;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CatalogList extends Model
+{
+    //
+}
---
+++ app/Src/ShippingMethod.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Src;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShippingMethod extends Model
+{
+    //
+}
